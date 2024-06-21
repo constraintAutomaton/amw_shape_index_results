1d3fb50815af5c6529ab11db10fad4ad740d5733
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-06-21 10:31:01 +0200

    hdt test

--- server/install.sh
+++ server/install.sh
@@ -1,10 +1,3 @@
-pushd rdf-dataset-fragmenter.js
-    yarn install --ignore-engines
-    yarn build || true
-    yarn unlink
-    yarn link
-popd
-
 pushd SolidBench.js
     yarn link "rdf-dataset-fragmenter"
     yarn install --ignore-engines
